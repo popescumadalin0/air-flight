3e1a6990c513b041c2545c126d3898106f28a803
DizzYForester <dizzyforester@gmail.com>
2024-04-29 16:56:26 +0300

    learning

---
+++ AirFlightsDashboard/AirFlights/Pages/Rezervare/Rezervare.razor.cs
@@ -0,0 +1,13 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+
+namespace AirFlightsClient.Pages.Rezervare
+{
+    public partial class Rezervare : BaseComponent
+    {
+        [Inject]
+        private NavigationManager Nav1 { get; set; }
+
+        private int selectedValue { get; set; }
+    }
+}
