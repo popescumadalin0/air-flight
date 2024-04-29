9ce818db56125b13baf1da68eea4fa46357b0983
DizzYForester <dizzyforester@gmail.com>
2024-04-29 16:23:41 +0300

    comit

---
+++ AirFlightsDashboard/AirFlights/Shared/NavMenu.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace AirFlightsClient.Shared
+{
+    public partial class NavMenu
+    {
+        private bool collapseNavMenu = true;
+
+        private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+
+        protected override void OnInitialized()
+        {
+
+            base.OnInitialized();
+        }
+    }
+}
