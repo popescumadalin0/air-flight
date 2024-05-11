f2d76eee26d4d6532c135729e0373eed4d13ef02
Janina <coceijanina@yahoo.com>
2024-05-11 11:28:04 +0300

    Register page

--- AirFlightsDashboard/AirFlights/Models/RegisterModel.cs
+++ AirFlightsDashboard/AirFlights/Models/RegisterModel.cs
@@ -5,8 +5,31 @@ namespace AirFlightsClient.Models;
 public class RegisterModel
 {
     [Required]
-    public string Username { get; set; }
+    public string CNP { get; set; }
 
     [Required]
+
     public string Password { get; set; }
+    [Required]
+
+    public string ConfirmPassword { get; set; }
+
+    [Required]
+
+    public string PhoneNumber { get; set; }
+
+    public byte[] ProfileImage { get; set; } 
+    
+
+    [Required]
+
+    public string FirstName { get; set; }
+
+    [Required]
+
+    public string LastName { get; set; }
+
+    [Required]
+
+    public string Email { get; set; }
 }
