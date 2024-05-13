083c5953198c76e0acfacac8b7b9c8840dbd158d
janina280 <coceijanina@yahoo.com>
2024-05-14 01:08:30 +0300

    Add Ticket design and remove js

--- AirFlightsDashboard/AirFlights/Pages/Booking/Booking.razor.cs
+++ AirFlightsDashboard/AirFlights/Pages/Booking/Booking.razor.cs
@@ -2,25 +2,25 @@ using System;
 using Blazorise;
 using Microsoft.AspNetCore.Components;
 
-namespace AirFlightsDashboard.Pages.Booking
+namespace AirFlightsDashboard.Pages.Booking;
+
+public partial class Booking : BaseComponent
 {
-    public partial class Booking : BaseComponent
-    {
-        [Inject]
-        private NavigationManager Nav1 { get; set; }
+    [Inject]
+    private NavigationManager Nav1 { get; set; }
 
-        private int selectedValue { get; set; }
-        bool accordionItem1Visible = true;
-        bool accordionItem2Visible = false;
-        bool accordionItem3Visible = false;
+    private int selectedValue { get; set; }
+    bool accordionItem1Visible = true;
+    bool accordionItem2Visible = false;
+    bool accordionItem3Visible = false;
+   
 
-        DatePicker<DateTime?> datePickerDestination;
+DatePicker<DateTime?> datePickerDestination;
 
-        DateTime? valueDestination;
+    DateTime? valueDestination;
 
-        DatePicker<DateTime?> datePickerReturn;
+    DatePicker<DateTime?> datePickerReturn;
 
-        DateTime? valueReturn;
+    DateTime? valueReturn;
 
-    }
 }
