6c643392b513d62b54f42649e627a65445788abd
janina280 <coceijanina@yahoo.com>
2024-05-14 18:23:16 +0300

    Add personal details page

---
+++ AirFlightsDashboard/AirFlights/Pages/Account/Account.razor.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using AirFlightsDashboard.Models;
+using AirFlightsDashboard.States;
+using Microsoft.AspNetCore.Components;
+
+namespace AirFlightsDashboard.Pages.Account;
+
+public partial class Account : ComponentBase, IDisposable
+{
+    [Inject]
+    private SnackbarState SnackbarState { get; set; }
+
+    [Inject]
+    private LoadingState LoadingState { get; set; }
+    public void Dispose()
+    {
+        SnackbarState.OnStateChange -= StateHasChanged;
+        LoadingState.OnStateChange += StateHasChanged;
+    }
+
+    protected override void OnInitialized()
+    {
+        SnackbarState.OnStateChange += StateHasChanged;
+        LoadingState.OnStateChange += StateHasChanged;
+    }
+
+
+}
